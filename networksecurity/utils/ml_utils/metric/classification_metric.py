64f3d2068d8e4db0732bfda513c44ef528f93c55
jtaddrocket <nguyentiendat3_t66@hus.edu.vn>
2025-11-07 20:59:30 +0700

    Update folder ml_utils

---
+++ networksecurity/utils/ml_utils/metric/classification_metric.py
@@ -0,0 +1,20 @@
+import sys
+
+from networksecurity.entity.artifact_entity import ClassificationModelTrainerArtifact
+from networksecurity.exception.exception import NetworkSecurityException
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+def get_classification_score(y_true, y_pred) -> ClassificationModelTrainerArtifact:
+    try:
+        model_f1_score = f1_score(y_true, y_pred)
+        model_recall_score = recall_score(y_true, y_pred)
+        model_f1_score = precision_score(y_true, y_pred)
+
+        classification_metric = ClassificationModelTrainerArtifact(
+            f1_score=model_f1_score,
+            precision_score=model_f1_score,
+            recall_score=model_recall_score
+        )
+        return classification_metric
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
