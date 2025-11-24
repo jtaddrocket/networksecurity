d258ef59f412dddeb10022b435a32a3de7102978
jtaddrocket <nguyentiendat3_t66@hus.edu.vn>
2025-11-24 10:59:42 +0700

    trainer evaluation and mlflow commited

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os 
 import sys 
+import mlflow
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -22,6 +23,7 @@ from sklearn.ensemble import (
     RandomForestClassifier
 )
 
+
 class ModelTrainer:
     def __init__(self,
                  model_trainer_config: ModelTrainerConfig,
@@ -31,7 +33,20 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
-        
+    
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision_score", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+
+            
+
     def train_model(self, X_train, y_train, X_test, y_test):
         models = {
             "Random Forest": RandomForestClassifier(verbose=1),
@@ -83,6 +98,7 @@ class ModelTrainer:
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
         # Track the MLFlow 
+        self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred = best_model.predict(X_test)
         classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
