f737b7fc1c766f8111f485483e89d0b616d163f9
jtaddrocket <nguyentiendat3_t66@hus.edu.vn>
2026-05-13 14:44:36 +0700

    Data Transformation Completed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,3 +1,5 @@
+import numpy
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
@@ -6,7 +8,9 @@ import sys
 import yaml 
 import dill
 import pickle 
-import pandas as pd 
+import numpy as np 
+import pandas as pd
+
 
 def read_yaml_file(file_path: str) -> dict:
     try:
@@ -23,5 +27,29 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w") as file:
             yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array(file_path: str, array: np.array) -> None:
+    """
+    Save numpy array data to file 
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
